ed4c8b6919c1e9ec27aa4727132f80fcfed635e4
45183 <hsb9743@naver.com>
2022-03-07 10:49:30 +0900

    day43

--- board/views.py
+++ board/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -19,13 +20,15 @@ def detail(request, question_id):
     context = {'question':question}
     return render(request, 'board/detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     # 질문 등록
     if request.method == "POST":
         form = QuestionForm(request.POST)       # 내용이 작성된 폼
         if form.is_valid():
             question = form.save(commit=False)      # 가저장
-            question.create_date = timezone.now()
+            question.create_date = timezone.now()   # 작성일
+            question.author = request.user          # 인증된 사용자(글쓴이)
             question.save()                         # 실제 저장
             return redirect('board:index')          # 질문 목록 페이지 강제 이동
     else:   # request.method == "GET"
@@ -33,6 +36,7 @@ def question_create(request):
     context = {'form':form}
     return render(request, 'board/question_form.html', context)   # 공통
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)      # url 경로 오류 처리
     # question = Question.objects.get(id=question_id) # 해당 질문 1개 가져옴
@@ -42,6 +46,7 @@ def answer_create(request, question_id):
             answer = form.save(commit=False)
             answer.create_date = timezone.now()
             answer.question = question
+            answer.author = request.user            # 인증된 사용자(글쓴이)
             answer.save()
             return redirect('board:detail', question_id=question_id)
     else:
