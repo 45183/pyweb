cfb71c3f1dd0465fea6fb56281f220f786e69331
45183 <hsb9743@naver.com>
2022-03-15 12:13:00 +0900

    a

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e78b#vjsf7h-!*y4ask$y!5e8tu-vu1i2^gq)ets-s+a-m8x=b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
